aa2558f2b39a5101615845e727d563c8ee0a1ee0
pukinidev <v.caro@uniandes.edu.co>
2024-10-19 03:04:17 -0500

    fix: 10.0.2.2 in flutter points to localhost!

--- lib/services/music/artist_service.dart
+++ lib/services/music/artist_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:spukify/models/artist.dart';
 
 class ArtistService {
-  final String _apiUrl = 'http://192.168.0.22:8080/artists';
+  final String _apiUrl = 'http://10.0.2.2:8080/artists';
   Future<List<Artist>> getArtists() async {
     final response = await http.get(Uri.parse(_apiUrl));
     if(response.statusCode == 200) {
